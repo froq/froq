24cf5ce16787ad63b7d526065195919b3b1bd289
Kerem Güneş <k-gun@mail.com>
2016-05-06 19:22:31 +0300

    Docs.

--- src/boot.php
+++ src/boot.php
@@ -1,20 +1,57 @@
 <?php
+/**
+ * Copyright (c) 2016 Kerem Güneş
+ *    <k-gun@mail.com>
+ *
+ * GNU General Public License v3.0
+ *    <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 ob_start();
 
-define('APP_START_TIME', microtime(true));
-
+/**
+ * Just for fun.
+ * @const null
+ */
 define('nil', null, true);
+
+/**
+ * More readable empty strings.
+ * @const string
+ */
 define('none', '', true);
 
+/**
+ * Used to detect local env.
+ * @const bool
+ */
 define('local', (
     isset($_SERVER['SERVER_NAME']) &&
         (bool) strstr($_SERVER['SERVER_NAME'], '.local')), true);
 
+/**
+ * Show all errors if local.
+ */
 if (local) {
     ini_set('display_errors', '1');
     ini_set('error_reporting', E_ALL);
 }
 
+/**
+ * Load global base files.
+ */
 require(__dir__ .'/global/def.php');
 require(__dir__ .'/global/cfg.php');
 require(__dir__ .'/global/fun.php');
@@ -22,4 +59,8 @@ require(__dir__ .'/global/fun.php');
 $autoload = require(__dir__ .'/Autoload.php');
 $autoload->register();
 
+/**
+ * Init app with default configs (comes from cfg.php).
+ * @return Froq\App
+ */
 return Froq\App::init($cfg);
