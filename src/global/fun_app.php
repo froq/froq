778e63ff3b970d67b23052375738fd469fed7154
Kerem Güneş <k-gun@mail.com>
2019-11-13 03:44:41 +0300

    global.fun_app.php: add app_env(),app_root(),app_config(),app_fail(),app_fails().

--- src/global/fun_app.php
+++ src/global/fun_app.php
@@ -22,10 +22,75 @@ function app_dir()
 }
 
 /**
+ * App env.
+ * @return string
+ * @since  4.0
+ */
+function app_env()
+{
+    return app()->env()->getName();
+}
+
+/**
+ * App root.
+ * @return string
+ * @since  4.0
+ */
+function app_root()
+{
+    return app()->root();
+}
+
+/**
  * App load time.
  * @return string
  */
 function app_load_time()
 {
     return app()->loadTime();
+}
+
+/**
+ * App config.
+ * @param  string|array $key
+ * @param  any          $value_default
+ * @return any|froq\config\Config
+ * @since  4.0
+ */
+function app_config($key, $value_default = null)
+{
+    return app()->config($key, $value_default);
+}
+
+/**
+ * App fail.
+ * @param  string   $name
+ * @param  any|none $value
+ * @return any|null
+ * @since  4.0
+ */
+function app_fail($name, $value = none)
+{
+    return ($value === none)
+        ? get_global('app.fail.'. $name)
+        : set_global('app.fail.'. $name, $value);
+}
+
+/**
+ * App fails.
+ * @return array
+ * @since  4.0
+ */
+function app_fails()
+{
+    $ret = [];
+
+    $fails = get_global('app.fail.*');
+    if ($fails) {
+        foreach ($fails as $name => $fail) {
+            $ret[$name] = $fail;
+        }
+    }
+
+    return $ret;
 }
