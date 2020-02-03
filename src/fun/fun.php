8f761526ade3b31b537dbe627703b4a2057759e0
Kerem Güneş <qeremy@gmail.com>
2020-02-04 00:54:05 +0300

    fun/fun: update size() [drop Traversable check].

--- src/fun/fun.php
+++ src/fun/fun.php
@@ -81,7 +81,6 @@ function size($input)
         if ($input instanceof stdClass)       return count((array) $input);
         if (method_exists($input, 'count'))   return $input->count();
         if (method_exists($input, 'toArray')) return count($input->toArray());
-        if ($input instanceof Traversable)    return count((array) iterator_to_array($input));
     }
 
     return null; // No valid input.
