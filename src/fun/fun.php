9f2ab4d0fc0827f6597b7d6012837cde818f4191
Kerem Güneş <qeremy@gmail.com>
2020-02-26 07:45:46 +0300

    fun.fun: make-ups..

--- src/fun/fun.php
+++ src/fun/fun.php
@@ -5,13 +5,13 @@
 
 /**
  * No.
- * @param  ... $vars
+ * @param  ... $ins
  * @return bool
  */
-function no(...$vars)
+function no(...$ins)
 {
-    foreach ($vars as $var) {
-        if (!$var || ($var instanceof stdClass && !((array) $var))) {
+    foreach ($ins as $in) {
+        if (!$in || ($in instanceof stdClass && !((array) $in))) {
             return true;
         }
     }
@@ -20,13 +20,13 @@ function no(...$vars)
 
 /**
  * Not.
- * @param  ... $vars
+ * @param  ... $ins
  * @return bool
  */
-function not(...$vars)
+function not(...$ins)
 {
-    foreach ($vars as $var) {
-        if ($var === false) {
+    foreach ($ins as $in) {
+        if ($in === false) {
             return true;
         }
     }
@@ -35,24 +35,24 @@ function not(...$vars)
 
 /**
  * Upper.
- * @param  any $input
+ * @param  any $in
  * @return string|null
  * @since  3.0
  */
-function upper($input)
+function upper($in)
 {
-    return is_string($input) ? mb_convert_case($input, MB_CASE_UPPER_SIMPLE) : null;
+    return is_string($in) ? mb_convert_case($in, MB_CASE_UPPER_SIMPLE) : null;
 }
 
 /**
  * Lower.
- * @param  any $input
+ * @param  any $in
  * @return string|null
  * @since  3.0
  */
-function lower($input)
+function lower($in)
 {
-    return is_string($input) ? mb_convert_case($input, MB_CASE_LOWER_SIMPLE) : null;
+    return is_string($in) ? mb_convert_case($in, MB_CASE_LOWER_SIMPLE) : null;
 }
 
 /**
@@ -60,26 +60,26 @@ function lower($input)
  * @aliasOf size()
  * @since   3.0
  */
-function len($input)
+function len($in)
 {
-    return size($input);
+    return size($in);
 }
 
 /**
  * Size.
- * @param  any $input
+ * @param  any $in
  * @return int|null
  * @since  3.0
  */
-function size($input)
+function size($in)
 {
-    if (is_string($input))    return strlen($input);
-    if (is_countable($input)) return count($input);
+    if (is_string($in))    return strlen($in);
+    if (is_countable($in)) return count($in);
 
-    if ($input && is_object($input)) {
-        if ($input instanceof stdClass)       return count((array) $input);
-        if (method_exists($input, 'count'))   return $input->count();
-        if (method_exists($input, 'toArray')) return count($input->toArray());
+    if ($in && is_object($in)) {
+        if ($in instanceof stdClass)       return count((array) $in);
+        if (method_exists($in, 'count'))   return $in->count();
+        if (method_exists($in, 'toArray')) return count($in->toArray());
     }
 
     return null; // No valid input.
@@ -87,19 +87,19 @@ function size($input)
 
 /**
  * Slice.
- * @param  array|string $input
+ * @param  array|string $in
  * @param  int          $start
  * @param  int|null     $end
  * @return array|string|null
  * @since  3.0, 4.0 Added back.
  */
-function slice($input, $start, $end = null)
+function slice($in, $start, $end = null)
 {
-    if (is_array($input)) {
-        return array_slice($input, $start, $end);
+    if (is_array($in)) {
+        return array_slice($in, $start, $end);
     }
-    if (is_string($input)) {
-        return mb_substr($input, $start, $end ?? mb_strlen($input));
+    if (is_string($in)) {
+        return mb_substr($in, $start, $end ?? mb_strlen($in));
     }
 
     return null; // No valid input.
@@ -107,49 +107,49 @@ function slice($input, $start, $end = null)
 
 /**
  * Strip.
- * @param  string      $input
+ * @param  string      $in
  * @param  string|null $chars
  * @return string
  * @since  3.0
  */
-function strip($input, $chars = null)
+function strip($in, $chars = null)
 {
-    if ($chars != null) {
-        // Regexp: only ~...~ patterns accepted.
+    if ($chars) {
+        // RegExp: only ~...~ patterns accepted.
         if (strlen($chars) >= 3 && $chars[0] == '~') {
             $rules = substr($chars, 1, ($pos = strrpos($chars, '~')) - 1);
             $modifiers = substr($chars, $pos + 1);
 
-            return preg_replace(sprintf('~^%s|%s$~%s', $rules, $rules, $modifiers), '', $input);
+            return preg_replace(sprintf('~^%s|%s$~%s', $rules, $rules, $modifiers), '', $in);
         }
 
-        return trim($input, $chars);
+        return trim($in, $chars);
     }
 
-    return trim($input);
+    return trim($in);
 }
 
 /**
  * Split (we missed you so much baby..).
- * @param  string   $delim
- * @param  string   $input
+ * @param  string   $sep
+ * @param  string   $in
  * @param  int|null $limit
  * @param  int|null $flags
  * @return array
  */
-function split($delim, $input, $limit = null, $flags = null)
+function split($sep, $in, $limit = null, $flags = null)
 {
-    // Regexp: only "~...~" patterns accepted.
-    $delimlen = strlen($delim);
-    if ($delimlen == 0) {
-        $delim = '~~u'; // Split all.
-        $delimlen = 3;
+    // RegExp: only "~...~" patterns accepted.
+    $seplen = strlen($sep);
+    if ($seplen == 0) {
+        $sep = '~~u'; // Split all.
+        $seplen = 3;
     }
 
-    if ($delimlen >= 3 && $delim[0] == '~') { // Regexp.
-        $ret = (array) preg_split($delim, $input, $limit ?? -1, $flags ?? 1); // 1=No empty.
+    if ($seplen >= 3 && $sep[0] == '~') { // RegExp.
+        $ret = (array) preg_split($sep, $in, $limit ?? -1, $flags ?? 1); // 1=No empty.
     } else {
-        $ret = (array) explode($delim, $input, $limit ?? PHP_INT_MAX);
+        $ret = (array) explode($sep, $in, $limit ?? PHP_INT_MAX);
         if ($flags === null) { // Null=no empty.
             $ret = array_filter($ret, 'strlen');
         }
@@ -165,71 +165,74 @@ function split($delim, $input, $limit = null, $flags = null)
 
 /**
  * Unsplit (fun function).
- * @param  string $delim
- * @param  array  $input
+ * @param  string $sep
+ * @param  array  $in
  * @return string
  * @since  3.0
  */
-function unsplit($delim, $input)
+function unsplit($sep, $in)
 {
-    return join($delim, $input);
+    return join($sep, $in);
 }
 
 /**
  * Remove.
- * @param  array|string $input
+ * @param  array|string $in
  * @param  any          $search
  * @return array|string|null
  * @since  3.0
  */
-function remove($input, $search)
+function remove($in, $search)
 {
-    return replace($input, $search, '', true);
+    return replace($in, $search, '', true);
 }
 
 /**
  * Replace.
- * @param  array|string $input
+ * @param  string|array $in
  * @param  any          $search
  * @param  any          $replacement
  * @param  bool         $remove
- * @return array|string|null
+ * @return string|array|null
  * @since  3.0
  */
-function replace($input, $search, $replacement, $remove = false)
+function replace($in, $search, $replacement, $remove = false)
 {
-    if (is_array($input)) {
-        $key = array_search($search, $input, true);
-        if ($key !== false) {
-            if ($remove) {
-                unset($input[$key]);
-            } else { $input[$key] = $replacement; }
+    if (is_string($in)) {
+        // RegExp: only ~...~ patterns accepted.
+        if (is_string($search) && strlen($search) >= 3 && $search[0] == '~') {
+            return !is_callable($replacement)
+                ? preg_replace($search, $replacement, $in)
+                : preg_replace_callback($search, $replacement, $in);
         }
-    } elseif (is_string($input)) {
-        if (is_string($search) && strlen($search) >= 3 && $search[0] == '~') { // Regexp.
-            $input = !is_callable($replacement)
-                ? preg_replace($search, $replacement, $input)
-                : preg_replace_callback($search, $replacement, $input);
-        } else {
-            $input = str_replace($search, $replacement, $input);
-        }
-    } else {
-        $input = null; // No valid input.
+
+        return str_replace($search, $replacement, $in);
     }
 
-    return $input;
+    if (is_array($in)) {
+        $key = array_search($search, $in, true);
+        if ($key !== false) {
+            $in[$key] = $replacement;
+            if ($remove) {
+                unset($in[$key]);
+            }
+        }
+        return $in;
+    }
+
+    return null; // No valid input.
 }
 
 /**
  * Grep.
- * @param  string $input
+ * @param  string $in
  * @param  string $pattern
  * @return string|null
  * @since  3.0
  */
-function grep($input, $pattern)
+function grep($in, $pattern)
 {
-    preg_match($pattern, $input, $match);
+    preg_match($pattern, $in, $match);
     if (isset($match[1])) {
         return $match[1];
     }
@@ -238,14 +241,14 @@ function grep($input, $pattern)
 
 /**
  * Grep all.
- * @param  string $input
+ * @param  string $in
  * @param  string $pattern
  * @return array<string>|null
  * @since  3.15
  */
-function grep_all($input, $pattern)
+function grep_all($in, $pattern)
 {
-    preg_match_all($pattern, $input, $matches);
+    preg_match_all($pattern, $in, $matches);
     if (isset($matches[1])) {
         foreach (array_slice($matches, 1) as $match) {
             $ret[] = $match[0] ?? null;
