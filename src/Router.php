e0b915ff59e05963b12e6547cbf6ebc8594fd926
Kerem Güneş <qeremy@gmail.com>
2020-03-08 04:43:53 +0300

    Nope..

--- src/Router.php
+++ src/Router.php
@@ -144,6 +144,7 @@ final class Router
             // Format named parameters if given.
             if (strpos($pattern, ':')) {
                 $pattern = preg_replace_callback(
+                    // Eg: /:id[\d] or /:tab{show|hide}
                     '~(?<!\?):(\w+)(?:(?<c>[\[\{])(.+?)[\]\}])?~',
                     function ($match) {
                         $name = $match[1];
