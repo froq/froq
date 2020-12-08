444afab9324b325794991a6aa06003c2cef96b27
Kerem Güneş <qeremy@gmail.com>
2020-12-08 23:52:52 +0300

    Move upper,lower,strip,split,unsplit into sugars.

--- src/fun/fun.php
+++ src/fun/fun.php
@@ -47,30 +47,6 @@ function not($in, ...$ins)
 }
 
 /**
- * Upper-caser.
- *
- * @param  any $in
- * @return string|null
- * @since  3.0
- */
-function upper($in)
-{
-    return is_string($in) ? strtoupper($in) : null;
-}
-
-/**
- * Lower-caser.
- *
- * @param  any $in
- * @return string|null
- * @since  3.0
- */
-function lower($in)
-{
-    return is_string($in) ? strtolower($in) : null;
-}
-
-/**
  * Length getter.
  *
  * @alias of size()
@@ -79,79 +55,6 @@ function lower($in)
 function len(...$args)
 {
     return size(...$args);
-}
-
-/**
- * Strip a string, with RegExp (~) option.
- *
- * @param  string      $in
- * @param  string|null $chars
- * @return string
- * @since  3.0
- */
-function strip($in, $chars = null)
-{
-    if ($chars) {
-        // RegExp: only ~..~ patterns accepted.
-        if (strlen($chars) >= 3 && $chars[0] == '~') {
-            $rules = substr($chars, 1, ($pos = strrpos($chars, '~')) - 1);
-            $modifiers = substr($chars, $pos + 1);
-
-            return preg_replace(sprintf('~^%s|%s$~%s', $rules, $rules, $modifiers), '', $in);
-        }
-
-        return trim($in, $chars);
-    }
-
-    return trim($in);
-}
-
-/**
- * Split, missed you so much baby..
- *
- * @param  string   $sep
- * @param  string   $in
- * @param  int|null $limit
- * @param  int|null $flags
- * @return array
- */
-function split($sep, $in, $limit = null, $flags = null)
-{
-    // RegExp: only "~..~" patterns accepted.
-    $seplen = strlen($sep);
-    if ($seplen == 0) {
-        $sep = '~~u'; // Split all.
-        $seplen = 3;
-    }
-
-    if ($seplen >= 3 && $sep[0] == '~') { // RegExp.
-        $ret = (array) preg_split($sep, $in, $limit ?? -1, $flags ?? 1); // 1=No empty.
-    } else {
-        $ret = (array) explode($sep, $in, $limit ?? PHP_INT_MAX);
-        if ($flags === null) { // Null=no empty.
-            $ret = array_filter($ret, 'strlen');
-        }
-    }
-
-    // Plus: prevent 'undefined index..' error.
-    if ($limit && $limit != count($ret)) {
-        $ret = array_pad($ret, $limit, null);
-    }
-
-    return $ret;
-}
-
-/**
- * Unsplit, a fun function.
- *
- * @param  string $sep
- * @param  array  $in
- * @return string
- * @since  3.0
- */
-function unsplit($sep, $in)
-{
-    return join($sep, $in);
 }
 
 /**
