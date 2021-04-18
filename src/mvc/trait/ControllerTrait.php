d7516230dd580361e92fd33c7457247c1387abb7
Kerem Güneş <qeremy@gmail.com>
2021-04-18 05:12:37 +0300

    mvc.trait.ControllerTrait: add exportControllerStuff().

--- src/mvc/trait/ControllerTrait.php
+++ src/mvc/trait/ControllerTrait.php
@@ -34,4 +34,25 @@ trait ControllerTrait
     {
         return $this->controller;
     }
+
+    /**
+     * Export supclass vars for subclasses.
+     *
+     * @param  array|string $vars
+     * @return array
+     */
+    public final function exportControllerStuff(array|string $vars = null): array
+    {
+        // Add most used defaults (controller & model).
+        $ret = [$this->controller, $this->controller->getModel()];
+
+        // Gather optional vars (that also may be dynamic vars).
+        if ($vars != null) {
+            foreach ((array) $vars as $var) {
+                $ret[] = $this->controller->{$var} ?? null;
+            }
+        }
+
+        return $ret;
+    }
 }
