ff9201de33e9bc9d0583cd8523f12e0c94230c06
Kerem Güneş <qeremy@gmail.com>
2021-04-27 09:02:09 +0300

    mvc.data.Repository: add db, sql(),query() & use DbTrait, TableTrait, ValidationTrait.

--- src/mvc/data/Repository.php
+++ src/mvc/data/Repository.php
@@ -9,9 +9,11 @@ namespace froq\mvc\data;
 
 use froq\mvc\Controller;
 use froq\mvc\trait\{ControllerTrait, ModelTrait};
+use froq\database\{Query, sql\Sql};
+use froq\database\trait\{DbTrait, TableTrait, ValidationTrait};
 
 /**
- * Repository.
+ * Repository
  *
  * Represents an entity which is extended by producers/providers or other data/database related classes.
  *
@@ -25,6 +27,10 @@ class Repository
     /** @see froq\mvc\trait\ControllerTrait */
     /** @see froq\mvc\trait\ModelTrait */
     use ControllerTrait, ModelTrait;
+
+    /** @see froq\database\trait\DbTrait */
+    /** @see froq\database\trait\TableTrait */
+    use DbTrait, TableTrait, ValidationTrait;
 
     /**
      * Constructor.
@@ -40,5 +46,32 @@ class Repository
         if ($model = $this->controller->getModel()) {
             $this->model = $model;
         }
+
+        // Set db property using model or registry.
+        $this->db = $model?->db() ?: registry()::get('@app')->database();
+    }
+
+    /**
+     * Init a `Sql` object with/without given params argument.
+     *
+     * @param  string     $in
+     * @param  array|null $params
+     * @return froq\database\sql\Sql
+     */
+    public final function sql(string $in, array $params = null): Sql
+    {
+        return $this->db->initSql($in, $params);
+    }
+
+    /**
+     * Init a `Query` object using self `$db` property, setting its "table" query with `$table` argument
+     * when provided or using self `$table` property.
+     *
+     * @param  string|null $table
+     * @return froq\database\Query
+     */
+    public final function query(string $table = null): Query
+    {
+        return $this->db->initQuery($table ?? $this->table ?? null);
     }
 }
