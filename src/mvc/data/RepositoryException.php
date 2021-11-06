00a274e922ca8e36ef4c0d64612114a2c24cb514
Kerem Güneş <qeremy@gmail.com>
2021-11-07 02:23:39 +0300

    mvc.data: add RepositoryException class.

---
+++ src/mvc/data/RepositoryException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq
+ */
+declare(strict_types=1);
+
+namespace froq\mvc\data;
+
+use froq\AppException;
+
+/**
+ * Repository Exception.
+ *
+ * @package froq\mvc\data
+ * @object  froq\mvc\data\RepositoryException
+ * @author  Kerem Güneş
+ * @since   5.0
+ */
+class RepositoryException extends AppException
+{}
