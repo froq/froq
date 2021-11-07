931e18cca03bccd48e894472eeb5844f8a0b94be
Kerem Güneş <qeremy@gmail.com>
2021-11-07 05:23:55 +0300

    mvc.data.Repository: use database.Repository as parent [drop all related stuff].

--- src/mvc/data/Repository.php
+++ src/mvc/data/Repository.php
@@ -10,8 +10,7 @@ namespace froq\mvc\data;
 use froq\mvc\data\DataException;
 use froq\mvc\Controller;
 use froq\mvc\trait\{ControllerTrait, ModelTrait};
-use froq\database\{Query, sql\Sql, entity\Manager};
-use froq\database\trait\{DbTrait, TableTrait, ValidationTrait, EntityManagerTrait};
+use froq\database\{Database, Repository as _Repository};
 
 /**
  * Repository.
@@ -23,17 +22,11 @@ use froq\database\trait\{DbTrait, TableTrait, ValidationTrait, EntityManagerTrai
  * @author  Kerem Güneş
  * @since   5.0
  */
-class Repository
+class Repository extends _Repository
 {
     /** @see froq\mvc\trait\ControllerTrait */
     /** @see froq\mvc\trait\ModelTrait */
     use ControllerTrait, ModelTrait;
-
-    /** @see froq\database\trait\DbTrait */
-    /** @see froq\database\trait\TableTrait */
-    /** @see froq\database\trait\ValidationTrait */
-    /** @see froq\database\trait\EntityManagerTrait */
-    use DbTrait, TableTrait, ValidationTrait, EntityManagerTrait;
 
     /**
      * Constructor.
@@ -66,31 +59,6 @@ class Repository
             'No db exists to deal, check `database` option in app config or pass $db argument'
         );
 
-        $this->db = $db;
-        $this->em = $em ?? new Manager($db);
-    }
-
-    /**
-     * Init a `Sql` object with/without given params argument.
-     *
-     * @param  string     $in
-     * @param  array|null $params
-     * @return froq\database\sql\Sql
-     */
-    public final function sql(string $in, array $params = null): Sql
-    {
-        return $this->db->initSql($in, $params);
-    }
-
-    /**
-     * Init a `Query` object using self `$db` property, setting its "table" query with `$table` argument
-     * when provided or using self `$table` property.
-     *
-     * @param  string|null $table
-     * @return froq\database\Query
-     */
-    public final function query(string $table = null): Query
-    {
-        return $this->db->initQuery($table ?? $this->table ?? null);
+        parent::__construct($db, $em ?? null);
     }
 }
