2e2f03574642f0d30523f3f46a6da054371dcb86
Kerem Güneş <qeremy@gmail.com>
2022-06-05 18:34:17 +0300

    * app.data: Add ObjectList [use for DataObjectList,ValueObjectList].

--- src/app/data/DataObjectList.php
+++ src/app/data/DataObjectList.php
@@ -7,8 +7,6 @@ declare(strict_types=1);
 
 namespace froq\app\data;
 
-use froq\pager\{Pager, PagerTrait};
-
 /**
  * A list class, for collecting DTO instances.
  *
@@ -17,31 +15,8 @@ use froq\pager\{Pager, PagerTrait};
  * @author  Kerem Güneş
  * @since   6.0
  */
-class DataObjectList extends \ItemList
+class DataObjectList extends ObjectList
 {
-    use PagerTrait;
-
-    /** @var bool */
-    public bool $convertItems = true;
-
-    /**
-     * Constructor.
-     *
-     * @param array                 $items
-     * @param froq\pager\Pager|null $pager
-     */
-    public function __construct(array $items = [], Pager $pager = null)
-    {
-        // Sniff item class for converting items to DTOs.
-        if ($this->convertItems && ($itemClass = $this->sniffItemClass())) {
-            $items = $this->convertItems($items, $itemClass);
-        }
-
-        parent::__construct($items);
-
-        $this->pager = $pager;
-    }
-
     /**
      * @override
      */
@@ -55,35 +30,6 @@ class DataObjectList extends \ItemList
             }
         }
 
-        return $items;
-    }
-
-    /**
-     * Sniff item class extracting name from subclass name.
-     */
-    private function sniffItemClass(): string|null
-    {
-        if (str_ends_with(static::class, 'List')) {
-            $class = substr(static::class, 0, -4);
-            if (class_exists($class)
-                && class_extends($class, DataObject::class)) {
-                return $class;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Convert items to DTO instances.
-     */
-    private function convertItems(array $items, string $itemClass): array
-    {
-        $object = new $itemClass;
-        foreach ($items as &$item) {
-            if (is_array($item)) {
-                $item = (clone $object)->update($item);
-            }
-        }
         return $items;
     }
 }
--- src/app/data/ValueObjectList.php
+++ src/app/data/ValueObjectList.php
@@ -7,8 +7,6 @@ declare(strict_types=1);
 
 namespace froq\app\data;
 
-use froq\pager\{Pager, PagerTrait};
-
 /**
  * A list class, for collecting VO instances.
  *
@@ -17,31 +15,8 @@ use froq\pager\{Pager, PagerTrait};
  * @author  Kerem Güneş
  * @since   6.0
  */
-class ValueObjectList extends \ItemList
+class ValueObjectList extends ObjectList
 {
-    use PagerTrait;
-
-    /** @var bool */
-    public bool $convertItems = true;
-
-    /**
-     * Constructor.
-     *
-     * @param array                 $items
-     * @param froq\pager\Pager|null $pager
-     */
-    public function __construct(array $items = [], Pager $pager = null)
-    {
-        // Sniff item class for converting items to DTOs.
-        if ($this->convertItems && ($itemClass = $this->sniffItemClass())) {
-            $items = $this->convertItems($items, $itemClass);
-        }
-
-        parent::__construct($items);
-
-        $this->pager = $pager;
-    }
-
     /**
      * @override
      */
@@ -55,35 +30,6 @@ class ValueObjectList extends \ItemList
             }
         }
 
-        return $items;
-    }
-
-    /**
-     * Sniff item class extracting name from subclass name.
-     */
-    private function sniffItemClass(): string|null
-    {
-        if (str_ends_with(static::class, 'List')) {
-            $class = substr(static::class, 0, -4);
-            if (class_exists($class)
-                && class_extends($class, ValueObject::class)) {
-                return $class;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Convert items to DTO instances.
-     */
-    private function convertItems(array $items, string $itemClass): array
-    {
-        $object = new $itemClass;
-        foreach ($items as &$item) {
-            if (is_array($item)) {
-                $item = (clone $object)->update($item);
-            }
-        }
         return $items;
     }
 }
