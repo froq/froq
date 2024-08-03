533b7d0bb66bc36f3bf6b3748021817a53c991eb
Kerem Güneş <1508306+krmgns@users.noreply.github.com>
2024-08-03 19:50:57 +0300

    Add app.data.OutputInterface interface.

---
+++ src/app/data/OutputInterface.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq
+ */
+namespace froq\app\data;
+
+/**
+ * Output interface to detect simple DTO definitions as action returns.
+ *
+ * @package froq\app\data
+ * @class   froq\app\data\OutputInterface
+ * @author  Kerem Güneş
+ * @since   7.0
+ */
+interface OutputInterface
+{}
